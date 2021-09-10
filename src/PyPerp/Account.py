7fe33f7dd8e8de33eb2b2f286eb3ea49d2f2e0b9
DeveloperInProgress <velnaveen99@gmail.com>
2021-09-10 17:22:12 +0000

    upload

---
+++ src/PyPerp/Account.py
@@ -0,0 +1,40 @@
+#TO-DO:
+#1.) implement all the functions
+#2.) look for all the ways to import a wallet
+#3.) create a seperate abstraction for wallet if necessary
+
+class Account:
+    def __init__(self):
+        pass 
+
+    @property
+    def layer1provider(self):
+        pass 
+
+    @layer1provider.setter
+    def layer1provider(self):
+        pass 
+
+    @property
+    def layer2provider(self):
+        pass 
+
+    @layer2provider.setter
+    def layer2provider(self):
+        pass 
+
+    @property
+    def layer1wallet(self):
+        pass 
+
+    @layer1wallet.setter
+    def layer1wallet(self):
+        pass 
+
+    @property
+    def layer2wallet(self):
+        pass 
+
+    @layer2wallet.setter
+    def layer2wallet(self):
+        pass
---
+++ src/PyPerp/MetaData.py
@@ -0,0 +1,17 @@
+class MetaData:
+    def __init__(self):
+        pass 
+
+    @property
+    def metadataStore(self):
+        pass 
+
+    @metadataStore.setter
+    def metadataStore(self):
+        pass 
+
+    def getContractAddress(self):
+        pass 
+
+    def getExtContractAddress(self):
+        pass 
