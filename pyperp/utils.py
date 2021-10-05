23ddc1c7fad3586cb73c8ca420f01e098a21189d
DeveloperInProgress <velnaveen99@gmail.com>
2021-10-05 08:35:48 +0000

    PEP-8 compliance

--- pyperp/utils.py
+++ pyperp/utils.py
@@ -17,14 +17,19 @@ def confirmTransferToLayer2(receipt, provider):
     meta = MetaData(provider.testnet)
     layer2AmbAbi = json.loads(pkgutil.get_data(__name__, "abi/AmbL2.json"))
     layer2AmbAddr = meta.getL2ExtContractAddress("ambBrideOnXDai")
-    layer2Amb = provider.l2.eth.contract(address=layer2AmbAddr, abi=layer2AmbAbi)
+    layer2Amb = provider.l2.eth.contract(
+        address=layer2AmbAddr,
+        abi=layer2AmbAbi
+    )
     methodId = "0x482515ce"
     for r in receipt:
         log = r["logs"]
         if log.topics[0][:10] == methodId:
             break
     fromMsgId = log.topics[1]
-    confirmationFilter = layer2Amb.events.AffirmationCompleted.createFilter(fromBlock="latest", argument_filters={'toMsgId': fromMsgId})
+    confirmationFilter = layer2Amb.events.AffirmationCompleted.createFilter(
+        fromBlock="latest", argument_filters={"toMsgId": fromMsgId}
+    )
     while True:
         for c in confirmationFilter:
             print("Transfer completed")
@@ -41,15 +46,20 @@ def confirmTransferToLayer1(reciept, provider):
     """
     meta = MetaData(provider.testnet)
     layer1AmbAbi = json.loads(pkgutil.get_data(__name__, "abi/AmbL1.json"))
-    lauer1AmbAddr = meta.getL1ExtContractAddress("ambBridgeOnEth")
-    layer1Amb = provider.l1.eth.contract(address=layer1AmbAddr, abi=layer1AmbAbi)
+    layer1AmbAddr = meta.getL1ExtContractAddress("ambBridgeOnEth")
+    layer1Amb = provider.l1.eth.contract(
+        address=layer1AmbAddr,
+        abi=layer1AmbAbi
+    )
     methodId = "0x520d2afd"
-    for r in receipt:
+    for r in reciept:
         log = r["logs"]
         if log.topics[0][:10] == methodId:
             break
     fromMsgId = log.topics[1]
-    confirmationFilter = layer1Amb.events.RelayedMessage.createFilter(fromBlock="latest", argument_filters={'toMsgId': fromMsgId})
+    confirmationFilter = layer1Amb.events.RelayedMessage.createFilter(
+        fromBlock="latest", argument_filters={"toMsgId": fromMsgId}
+    )
     while True:
         for c in confirmationFilter:
             print("Transfer completed")
@@ -70,7 +80,7 @@ def getAmm(amm, provider):
         Amms = constants.AvailableAmms["mainnet"]
 
     if amm not in Amms:
-        raise ValueError(f"Unknown pair: {pair}")
+        raise ValueError(f"Unknown pair: {amm}")
 
     meta = MetaData.MetaData(provider.testnet)
     AmmAddr = meta.getL2ContractAddress(amm)
@@ -90,22 +100,35 @@ def estimateLiquidationPrice(position, amm, clearingHouse):
     clearingHouse: web3.eth.contract object for ClearingHouse
     """
     spotPrice = amm.functions.getSpotPricer.call().d
-    realCloseQuoteAmount = amm.functions.getOutputPrice(
-        0 if position.size.d > 0 else 1,
-        json.dumps(f"d: {abs(position.size.d)}")
-    ).call().d
-    maintanenceMarginRatio = clearingHouse.functions.maintanenceMarginRatio().call()
+    realCloseQuoteAmount = (
+        amm.functions.getOutputPrice(
+            0 if position.size.d > 0 else 1,
+            json.dumps(f"d: {abs(position.size.d)}")
+        )
+        .call()
+        .d
+    )
+    maintanenceMarginRatio = clearingHouse.functions.maintanenceMarginRatio(
+    ).call()
     openNotional = position.openNotional.d
     positionSizeAbs = abs(position.size.d)
     margin = position.margin.d
-    entryPrice = openNotional/positionSizeAbs
-    reverseLeverage = margin/openNotional
-    spotCloseQuoteAmount = spotPrice*positionSizeAbs
-    closePosPriceSlippage = (realCloseQuoteAmount - spotCloseQuoteAmount)/spotCloseQuoteAmount
+    entryPrice = openNotional / positionSizeAbs
+    reverseLeverage = margin / openNotional
+    spotCloseQuoteAmount = spotPrice * positionSizeAbs
+    closePosPriceSlippage = (
+        realCloseQuoteAmount - spotCloseQuoteAmount
+    ) / spotCloseQuoteAmount
     if position.size.d > 0:
-        liquidationPrice = entryPrice*(1-reverseLeverage-closePosPriceSlippage+maintanenceMarginRatio)
+        liquidationPrice = entryPrice * (
+            1 - reverseLeverage - closePosPriceSlippage
+            + maintanenceMarginRatio
+        )
     else:
-        liquidationPrice = entryPrice*(1+reverseLeverage-closePosPriceSlippage-maintanenceMarginRatio)
+        liquidationPrice = entryPrice * (
+            1 + reverseLeverage - closePosPriceSlippage
+            - maintanenceMarginRatio
+        )
     return liquidationPrice
 
 
@@ -115,9 +138,9 @@ def parseUnits(amount, decimal=constants.DEFAULT_DECIMALS):
 
     Arguments:
     amount -- the value to be parsed
-    decimal -- number of decimal units to parse the value to. Defaults to constans.DEFAULT_DECIMAL
+    decimal -- number of decimal units to parse the value to.
     """
-    return amount*(10**decimal)
+    return amount * (10 ** decimal)
 
 
 def formatUnits(amount, decimal=constants.DEFAULT_DECIMALS):
@@ -126,9 +149,9 @@ def formatUnits(amount, decimal=constants.DEFAULT_DECIMALS):
 
     Arguments:
     amount -- the value to be formatted
-    decimal -- number of decimal units to format the value to. Defaults to constans.DEFAULT_DECIMAL
+    decimal -- number of decimal units to format the value to.
     """
-    return amount/(10**decimal)
+    return amount / (10 ** decimal)
 
 
 def estimatedFundingRate(amm):
@@ -140,11 +163,13 @@ def estimatedFundingRate(amm):
     """
     durationFromSharp = datetime.datetime.now().minute * 60
     twapPrice = amm.functions.getTwapPrice(durationFromSharp).call()
-    underlyingTwapPrice = amm.functions.getUnderlyingTwapPrice(durationFromSharp).call()
+    underlyingTwapPrice = amm.functions.getUnderlyingTwapPrice(
+        durationFromSharp
+    ).call()
     fundingPeriod = amm.functions.fundingPeriod().call()
-    oneDayInSec = 60*60*24
+    oneDayInSec = 60 * 60 * 24
     marketTwapPrice = formatUnits(twapPrice[0])
     indexTwapPrice = formatUnits(underlyingTwapPrice[0])
     premium = marketTwapPrice - indexTwapPrice
-    premiumFraction = premium*fundingPeriod/oneDayInSec
-    return parseUnits(premiumFraction/indexTwapPrice)
+    premiumFraction = premium * fundingPeriod / oneDayInSec
+    return parseUnits(premiumFraction / indexTwapPrice)
