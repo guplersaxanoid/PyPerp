e047cc2d1da6d0d2fa7a568e0e73eaed2968fd39
DeveloperInProgress <velnaveen99@gmail.com>
2021-10-18 19:49:40 +0000

    v0.1.9

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('./README.md') as readme:
 setup(
     name = 'pyperp',        
     packages = ['pyperp'],  
-    version = '0.1.8',      
+    version = '0.1.9',      
     license='MIT',        
     description = 'python SDK for Perpetual Protocol',
     long_description_content_type='text/markdown',
